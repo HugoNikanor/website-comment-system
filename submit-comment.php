94bafeb2ee060a455c13f790315ad32674cf430d
Hugo <hugo.hornquist@gmail.com>
2016-03-25 19:15:03 +0100

    removed a bunch of unused echo:s

--- submit-comment.php
+++ submit-comment.php
@@ -4,21 +4,9 @@
 	$comment   = htmlspecialchars( $_POST["comment"]  );
 	$returnAdr = $_POST["returnAdr"];
 
-	echo "something";
-
-	echo $returnAdr;
-
 	require("./database-interface.php");
 
-	echo " else";
-
 	postCommentToDatabase( $filename, $author, $comment );
-
-
-	echo " is going on here...<br>";
-
-	echo $returnAdr;
-
 
 	header("Location: ".$returnAdr);
 	die();
