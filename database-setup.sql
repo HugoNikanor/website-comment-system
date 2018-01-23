d6bd694170ad3ceb5221444782eff68a69978b87
Hugo Hörnquist <hugo@hornquist.se>
2018-01-23 13:53:44 +0100

    changed database-setup to create both prod and test table

--- database-setup.sql
+++ database-setup.sql
@@ -1,4 +1,20 @@
-CREATE TABLE comments
+CREATE database IF NOT EXISTS website_comments;
+
+USE website_comments;
+
+CREATE TABLE IF NOT EXISTS blog_prod
+(
+	id       int(10)        NOT  NULL auto_increment,
+	entry    varchar(255)   NOT  NULL,
+	author   varchar(40)    NOT  NULL,
+	time     timestamp      NOT  NULL DEFAULT CURRENT_TIMESTAMP,
+	comment  varchar(2047)  NOT  NULL,
+	parent   int(10)        NOT  NULL DEFAULT 0,
+
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS blog_test
 (
 	id       int(10)        NOT  NULL auto_increment,
 	entry    varchar(255)   NOT  NULL,
